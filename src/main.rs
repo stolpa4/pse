0d3ce7ceab0889afb2c4a64e3a5a8b0a34c04969
stolpa4 <stolpa413@gmail.com>
2024-12-20 17:29:16 +0300

    Parsing args

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,25 @@ use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
 
+struct Arguments {
+    path: PathBuf,
+}
+
 fn main() {
-    let path = parse_arguments();
-    println!("Specified path: {}", path.display());
+    let args = parse_arguments();
+    println!("Specified path: {}", args.path.display());
     println!("GoodBye!");
 }
 
-
-fn parse_arguments() -> PathBuf {
+fn parse_arguments() -> Arguments {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: {} <path>", args[0]);
         std::process::exit(1);
     }
 
-    resolve_path(Path::new(&args[1]))
+    let path = resolve_path(Path::new(&args[1]));
+    Arguments { path }
 }
 
 fn resolve_path(path: &Path) -> PathBuf {
@@ -25,6 +29,6 @@ fn resolve_path(path: &Path) -> PathBuf {
         Err(e) => {
             eprintln!("Failed to resolve path: {}", e);
             std::process::exit(1);
-        },
+        }
     }
 }
