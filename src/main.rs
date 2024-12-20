65e1a13995ef5dc7eaa44233be32988d7fa6562a
stolpa4 <stolpa413@gmail.com>
2024-12-20 17:03:39 +0300

    Minor fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path>", args[0]);
+        std::process::exit(1);
+    }
+
+    let path = resolve_path(Path::new(&args[1]));
+
+    println!("Specified path: {}", path.display());
+
+    println!("GoodBye!");
+}
+
+
+fn resolve_path(path: &Path) -> PathBuf {
+    match fs::canonicalize(path) {
+        Ok(resolved) => resolved,
+        Err(e) => {
+            eprintln!("Failed to resolve path: {}", e);
+            std::process::exit(1);
+        },
+    }
 }
