8d9d99b3aedcdb66400c170c3cf2bf24b530ccff
stolpa4 <stolpa413@gmail.com>
2024-12-20 20:11:23 +0300

    Refactoring -> proper error handling

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,26 @@ struct Arguments {
 }
 
 fn main() {
-    let args = parse_arguments();
+    let args = match parse_arguments() {
+        Ok(args) => args,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            std::process::exit(1);
+        }
+    };
+
     println!("Specified path: {}", args.path.display());
     println!("GoodBye!");
 }
 
-fn parse_arguments() -> Arguments {
+fn parse_arguments() -> Result<Arguments, String> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: {} <path>", args[0]);
-        std::process::exit(1);
+        return Err(format!("Usage: {} <path>", args[0]));
     }
 
-    let path = resolve_path(Path::new(&args[1]));
-    Arguments { path }
-}
-
-fn resolve_path(path: &Path) -> PathBuf {
-    match fs::canonicalize(path) {
-        Ok(resolved) => resolved,
-        Err(e) => {
-            eprintln!("Failed to resolve path: {}", e);
-            std::process::exit(1);
-        }
+    match fs::canonicalize(Path::new(&args[1])) {
+        Ok(path) => Ok(Arguments { path }),
+        Err(e) => Err(format!("Failed to resolve path: {}", e)),
     }
 }
