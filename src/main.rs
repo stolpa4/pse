3af319e42c7d7a29c3dc44a66cc5c1b1cb6ac52d
stolpa4 <stolpa413@gmail.com>
2024-12-20 17:08:38 +0300

    Factoring out arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 fn main() {
+    let path = parse_arguments();
+    println!("Specified path: {}", path.display());
+    println!("GoodBye!");
+}
+
+
+fn parse_arguments() -> PathBuf {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: {} <path>", args[0]);
         std::process::exit(1);
     }
 
-    let path = resolve_path(Path::new(&args[1]));
-
-    println!("Specified path: {}", path.display());
-
-    println!("GoodBye!");
+    resolve_path(Path::new(&args[1]))
 }
-
 
 fn resolve_path(path: &Path) -> PathBuf {
     match fs::canonicalize(path) {
