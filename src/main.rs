bed502dc63bc72a9636cabd1495318e5735ad8d0
stolpa4 <stolpa413@gmail.com>
2024-12-22 07:35:05 +0300

    Parsing mode

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use std::collections::VecDeque;
 use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::collections::VecDeque;
+
+#[derive(Debug)]
+enum Mode {
+    Plain,
+    Content,
+    Recursive,
+}
 
 struct Arguments {
     path: PathBuf,
+    mode: Mode,
 }
 
 fn main() {
@@ -15,18 +23,42 @@ fn main() {
 
     let size = calculate_size(&args.path);
 
+    println!("Mode: {:?}", args.mode);
     println!("Path: {}, size: {} bytes", args.path.display(), size);
 }
 
 fn parse_arguments() -> Result<Arguments, String> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        return Err(format!("Usage: {} <path>", args[0]));
+        return Err(format!(
+            "Usage: {} <path> [mode(plain/content/recursive, default - plain)]",
+            args[0]
+        ));
     }
 
-    match fs::canonicalize(Path::new(&args[1])) {
-        Ok(path) => Ok(Arguments { path }),
-        Err(e) => Err(format!("Failed to resolve path: {}", e)),
+    let path = match fs::canonicalize(Path::new(&args[1])) {
+        Ok(path) => path,
+        Err(e) => return Err(format!("Failed to resolve path: {}", e)),
+    };
+
+    let mode = if args.len() > 2 {
+        parse_mode(&args[2])?
+    } else {
+        Mode::Plain // Default
+    };
+
+    Ok(Arguments { path, mode })
+}
+
+fn parse_mode(mode_str: &str) -> Result<Mode, String> {
+    match mode_str.to_lowercase().as_str() {
+        "plain" => Ok(Mode::Plain),
+        "content" => Ok(Mode::Content),
+        "recursive" => Ok(Mode::Recursive),
+        _ => Err(format!(
+            "Invalid mode: {}. Use 'plain', 'content', or 'recursive'.",
+            mode_str
+        )),
     }
 }
 
@@ -39,7 +71,7 @@ fn calculate_size(starting_path: &Path) -> u64 {
         let (size, subdirs) = process_path(&path);
         total_size += size;
         dirs_to_visit.extend(subdirs);
-     }
+    }
 
     total_size
 }
