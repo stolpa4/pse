a0d773954fd857f8575abe284cc9a7146ed77e23
stolpa4 <stolpa413@gmail.com>
2024-12-25 22:18:58 +0300

    Not sure how to use it for now

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,5 +1,7 @@
 use crate::proc::{FsEntry, FsTree};
 use crate::utils::size_to_label;
+use std::collections::BTreeMap;
+use std::cmp::Reverse;
 use serde::ser::{Serialize, SerializeMap, Serializer};
 use serde_json;
 use std::fs;
@@ -7,12 +9,30 @@ use std::fs::File;
 use std::io;
 use std::path::Path;
 
+impl FsEntry {
+    fn convert_to_btreemap(& self) -> BTreeMap<(Reverse<u64>, &String), &FsEntry> {
+        let mut map = BTreeMap::new();
+        if let FsEntry::Directory(ref directory) = *self {
+            for (name, entry) in &directory.content {
+                let size = match entry {
+                    FsEntry::File(f) => f.size,
+                    FsEntry::Directory(d) => d.size,
+                };
+                // NOTE: Use Reverse to sort by descending order
+                map.insert((Reverse(size), name), entry);
+            }
+        }
+        map
+    }
+}
+
+
 impl Serialize for FsEntry {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        match *self {
+        match self {
             FsEntry::File(ref file) => {
                 let mut state = serializer.serialize_map(Some(2))?;
                 state.serialize_entry("type", "file")?;
@@ -21,11 +41,12 @@ impl Serialize for FsEntry {
                 state.end()
             }
             FsEntry::Directory(ref directory) => {
+                let sorted_contents = self.convert_to_btreemap();
                 let mut state = serializer.serialize_map(Some(3))?;
                 state.serialize_entry("type", "directory")?;
                 state.serialize_entry("path", &directory.path)?;
                 state.serialize_entry("size", &size_to_label(directory.size))?;
-                state.serialize_entry("contents", &directory.content)?;
+                state.serialize_entry("contents", &sorted_contents)?;
                 state.end()
             }
         }
